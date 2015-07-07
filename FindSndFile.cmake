a15408d1d498dfd6571e43682b309043ebd65d14
Marrub <marrub@greyserv.net>
2015-07-07 00:19:13 -0400

    oops

--- FindSndFile.cmake
+++ FindSndFile.cmake
@@ -1,67 +1,29 @@
-# Found on http://hg.kvats.net
-#
-# - Try to find libsndfile
-# 
+# - Try to find SndFile
 # Once done this will define
 #
-#  SNDFILE_FOUND - system has libsndfile
-#  SNDFILE_INCLUDE_DIRS - the libsndfile include directory
-#  SNDFILE_LIBRARIES - Link these to use libsndfile
+#  SNDFILE_FOUND - system has SndFile
+#  SNDFILE_INCLUDE_DIRS - the SndFile include directory
+#  SNDFILE_LIBRARIES - Link these to use SndFile
 #
-#  Copyright (C) 2006  Wengo
+#  Copyright © 2006  Wengo
+#  Copyright © 2009 Guillaume Martres
 #
 #  Redistribution and use is allowed according to the terms of the New
 #  BSD license.
 #  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
 
-if (SNDFILE_LIBRARIES AND SNDFILE_INCLUDE_DIRS)
-  # in cache already
-  set(SNDFILE_FOUND TRUE)
-else (SNDFILE_LIBRARIES AND SNDFILE_INCLUDE_DIRS)
+find_path(SNDFILE_INCLUDE_DIR NAMES sndfile.h)
 
-  find_path(SNDFILE_INCLUDE_DIR
-    NAMES
-      sndfile.h
-    PATHS
-      /usr/include
-      /usr/local/include
-      /opt/local/include
-      /sw/include
-  )
-  
-  find_library(SNDFILE_LIBRARY
-    NAMES
-      sndfile
-    PATHS
-      /usr/lib
-      /usr/local/lib
-      /opt/local/lib
-      /sw/lib
-  )
+find_library(SNDFILE_LIBRARY NAMES sndfile sndfile-1)
 
-  set(SNDFILE_INCLUDE_DIRS
-    ${SNDFILE_INCLUDE_DIR}
-  )
-  set(SNDFILE_LIBRARIES
-    ${SNDFILE_LIBRARY}
-  )
+set(SNDFILE_INCLUDE_DIRS ${SNDFILE_INCLUDE_DIR})
+set(SNDFILE_LIBRARIES ${SNDFILE_LIBRARY})
 
-  if (SNDFILE_INCLUDE_DIRS AND SNDFILE_LIBRARIES)
-    set(SNDFILE_FOUND TRUE)
-  endif (SNDFILE_INCLUDE_DIRS AND SNDFILE_LIBRARIES)
+INCLUDE(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set SNDFILE_FOUND to TRUE if
+# all listed variables are TRUE
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SndFile DEFAULT_MSG SNDFILE_LIBRARY SNDFILE_INCLUDE_DIR)
 
-  if (SNDFILE_FOUND)
-    if (NOT SndFile_FIND_QUIETLY)
-      message(STATUS "Found libsndfile: ${SNDFILE_LIBRARIES}")
-    endif (NOT SndFile_FIND_QUIETLY)
-  else (SNDFILE_FOUND)
-    if (SndFile_FIND_REQUIRED)
-      message(FATAL_ERROR "Could not find libsndfile")
-    endif (SndFile_FIND_REQUIRED)
-  endif (SNDFILE_FOUND)
-
-  # show the SNDFILE_INCLUDE_DIRS and SNDFILE_LIBRARIES variables only in the advanced view
-  mark_as_advanced(SNDFILE_INCLUDE_DIRS SNDFILE_LIBRARIES)
-
-endif (SNDFILE_LIBRARIES AND SNDFILE_INCLUDE_DIRS)
+# show the SNDFILE_INCLUDE_DIRS and SNDFILE_LIBRARIES variables only in the advanced view
+mark_as_advanced(SNDFILE_INCLUDE_DIRS SNDFILE_LIBRARIES)
