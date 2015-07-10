8e90381b9a58dca1b2bb69f735a15235c19d47bf
Marrub <marrub@greyserv.net>
2015-07-10 05:05:00 +0000

    this file doesn't contain any code by id anymore

--- src/st_stuff.cpp
+++ src/st_stuff.cpp
@@ -3,17 +3,25 @@
 //
 // $Id:$
 //
-// Copyright (C) 1993-1996 by id Software, Inc.
+// Copyright (c) 2015 Graham Sanderson <marrub@greyserv.net>
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 //
 // $Log:$
 //
@@ -247,7 +255,7 @@ static bool CheckCmdCheat(const char *cmd)
 	const size_t len = end - beg;
 
 	int i;
-	for(i = countof(CmdCheatCommands)-1; i >= 0; --i)
+	for(i = countof(CmdCheatCommands) - 1; i >= 0; --i)
 	{
 		if(strnicmp(beg, CmdCheatCommands[i], len) == 0 && CmdCheatCommands[i][len] == 0)
 		{
