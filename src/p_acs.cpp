2bb909a8d59cc3ac0493a816fb43fa343fa916b4
Marrub <marrub@greyserv.net>
2015-07-31 17:20:31 -0400

    merge zdoom/d61c48d

--- src/p_acs.cpp
+++ src/p_acs.cpp
@@ -9277,7 +9277,7 @@ scriptwait:
 			break;
 
 		case PCD_PLAYERINGAME:
-			if (STACK(1) < 0 || STACK(1) > MAXPLAYERS)
+			if (STACK(1) < 0 || STACK(1) >= MAXPLAYERS)
 			{
 				STACK(1) = false;
 			}
@@ -9288,7 +9288,7 @@ scriptwait:
 			break;
 
 		case PCD_PLAYERISBOT:
-			if (STACK(1) < 0 || STACK(1) > MAXPLAYERS || !playeringame[STACK(1)])
+			if (STACK(1) < 0 || STACK(1) >= MAXPLAYERS || !playeringame[STACK(1)])
 			{
 				STACK(1) = false;
 			}
